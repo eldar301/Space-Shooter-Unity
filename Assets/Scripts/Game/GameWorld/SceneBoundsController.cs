2e1bc9d93b0263b4981cf1d351e1c2fe6e6ab0bc
Eldar Goloviznin <radleweird@gmail.com>
2017-04-10 14:59:18 +0300

    camera behavior changes

---
+++ Assets/Scripts/Game/GameWorld/SceneBoundsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneBoundsController : MonoBehaviour {
+
+	[SerializeField] private float width;
+	[SerializeField] private float height;
+
+	public float GetHeight() {
+		return height;
+	}
+
+	public float GetWidth() {
+		return width;
+	}
+}
